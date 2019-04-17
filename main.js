af5b6852b0b5313584b36fbf207d86abef73fa71
Den <den.matvssnko.99@ukr.net>
2019-04-18 00:08:01 +0300

    new site

---
+++ main.js
@@ -0,0 +1,31 @@
+const main = document.getElementById("main");
+const infoElem = document.getElementById("info-elem");
+let cloneProject;
+
+main.addEventListener("click", (event)=>{
+	let project = event.target;
+	while(!project.classList.contains("project")) {
+		if(project === document.documentElement) break
+		project = project.parentNode;
+	}
+	if(!project.classList.contains("project") || document.documentElement.getBoundingClientRect().width < 320 ) return
+	if(infoElem.style.right == "0%") {
+		infoElem.style.right = "-100%";
+		
+		main.removeChild(cloneProject);
+		return
+	}
+	cloneProject = project.cloneNode(true);
+
+	cloneProject.style.position = "absolute";
+	cloneProject.style.top = project.getBoundingClientRect().top - main.getBoundingClientRect().top + "px";
+	cloneProject.style.left = project.getBoundingClientRect().left + "px";
+	cloneProject.style.transition = ".4s";
+	setTimeout(()=>{
+		cloneProject.style.left = 0;
+	}, 1);
+
+	main.appendChild(cloneProject);
+
+	infoElem.style.right = "0%";
+}, false)
